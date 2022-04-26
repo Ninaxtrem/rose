901c6ec979c1c7b2492f3ca0e8fb42715861c199
Ninaxtrem <nina@starxtrem.com>
2022-04-26 13:33:24 +0200

    reponsive

---
+++ assets/js/ongle.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.slide').slick({
+        "setting-name": "setting-value"
+    });
+});
+
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
