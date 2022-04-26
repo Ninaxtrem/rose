2d9ea7736f7869c015436bccd289a744ccfd3a36
BadbooX <k.cotton.pro@gmail.com>
2022-04-27 00:29:49 +0200

    traitement_connex 0.0.1

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
+    
+  
   <title>Rose Etern'ails</title>
 </head>
 
@@ -41,6 +41,10 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
+              </li>
+              <!-- Link panel admin -->
+              <li class="nav-item">
+                <a class="nav-link" href="connex_admin.php">X</i></a>
               </li>
             </ul>
           </div>
@@ -195,7 +199,7 @@
 
   <!-- fin foother-->
 
-
+  
   <script src="assets/js/ongle.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
@@ -203,9 +207,11 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
   </script>
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
   </script>
+  
 </body>
 
 </html>
